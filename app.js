3f23a45cc1e0d8f87fd0f8fa6a0f5c11ac9b40f1
jgraykeyin <justin.gray@keyin.com>
2021-02-19 11:48:33 -0330

    Added sound effects on button-press

--- app.js
+++ app.js
@@ -11,23 +11,47 @@ async function fetchJSONData() {
     console.log(things[num]["action"]);
 
     document.getElementById("output-text").innerHTML = `<p>${things[num]['action']}</p>`;
-
-
 }
-
 
 function main() {
 
     console.log("Hey!");
 
+    // Setup sound effect files
+    let snd_spin = new sound("audio/spin1.m4a");
+    let snd_done = new sound("audio/done1.m4a");
+
     refresh_btn = document.getElementById("activity-button");
     refresh_btn.addEventListener("click", function() {
+        
+        snd_spin.play();
         let timerId = setInterval(() => {
             fetchJSONData();
-        }, 50);
+        }, 57);
 
-        setTimeout(() => { clearInterval(timerId); console.log('stop'); }, 2000);
+        setTimeout(() => { clearInterval(timerId); snd_done.play(); }, 2000);
+        
     });
+}
+
+// Object to create a new audio element for sound effects to play
+class sound {
+    constructor(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+    }
+
+    play() {
+        this.sound.play();
+    }
+
+    pause() {
+        this.sound.pause();
+    }
 }
 
 
